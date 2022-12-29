00245372e7bf29d646014003d2c17c97d9a8458c
KirillMonster <kirill_isaev_2008@mail.ru>
2022-12-29 23:26:12 +0500

    .

--- GyverLamp2/exceptions.py
+++ GyverLamp2/exceptions.py
@@ -1,14 +1,13 @@
 class GyverLamp2Error(Exception):
-    """ Basic exception when working with the Club House """
     pass
 
 
 class CountEffectsNotInRangeError(GyverLamp2Error):
     def __init__(self):
-        super().__init__('Количество эффектов должно быть в диапозоне 0 и 26')
+        super().__init__('Количество эффектов должно быть в диапозоне от 0 до 26')
 
 
 class SyncError(GyverLamp2Error):
     def __init__(self):
         super().__init__('Ошибка синхронизации лампы. Проверьте что вы находитесь в одной сети и у лампы стоит '
-                         'модифицированная прошивка https://github.com/AlexGyver/GyverLamp#chapter-0')
+                         'модифицированная прошивка - https://github.com/KirillMonster/GyverLamp2#sync-system')
