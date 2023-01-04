915e76db28f991378dc3e28ebab5f8f3804fd5b0
KirillMonster <kirill_isaev_2008@mail.ru>
2023-01-04 18:09:57 +0500

    refactoring

--- examples/sync.py
+++ examples/sync.py
@@ -1,8 +1,6 @@
 from GyverLamp2 import Lamp
 
 lamp = Lamp(ip='192.168.1.237', log_data_request=True)
-print(lamp.ip, lamp.port, lamp.local_ip)
 lamp.sync_settings()
-print('SUCCESSFULLY')
-print('Client address', lamp.client_address)
-# lamp.settings(brightness=255)  # Отправляем теперь свои настройки
+print('Адрес ESP:', lamp.client_address)
+lamp.settings(min_brightness=100, max_brightness=255, brightness=255)  # Отправляем теперь свои настройки
